745c6f878df98f0bc064846f4f0507f2b5d08bb1
Anderson Mattar <andmat7745@gmail.com>
2023-05-22 13:17:50 -0300

    Alterações dia 22 de maio 2023

--- ALLtecSolutions/alltech_solutions/src/App.js
+++ ALLtecSolutions/alltech_solutions/src/App.js
@@ -2,18 +2,25 @@
 import './App.css';
 
 function App() {
+const saudação = 'seja bem vindo!' 
+
   return (
     <div className="App">
       <header className="App-header">
-           
-      <div className="App-logo">
-    <img  src='./alltecvideo.jpg' alt='logo alltech' title="Alltech" className='App-image'/>
-    </div>
 
-         <h1 className="App-title">Site All Tech</h1>       
-        
+        <div className="App-logo">
+          <img src='./alltecvideo.jpg' alt='logo alltech' title="Alltech" className='App-image'/>
+
+          <p>Olá, {saudação}</p>
+          <h1 className="App-title">All Tech -- Site em construção!</h1>
+         
+
+        </div>
+
+
+
       </header>
-    
+
 
     </div>
   );
