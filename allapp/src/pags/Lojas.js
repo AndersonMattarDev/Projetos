358f71165a34e830f2ece6074dfe3f9a241f63cd
Anderson Mattar <andmat7745@gmail.com>
2023-07-19 17:20:32 -0300

    texto da pagina lojas

--- allapp/src/pags/Lojas.js
+++ allapp/src/pags/Lojas.js
@@ -5,11 +5,12 @@ function Lojas() {
     return(
         <div className={styles.conteudo}>
             <section className={styles.titulo}>
-            <h1>Sejam Bem Vindos</h1>
+            <p className={styles.typingAnimation}>Mostre seus produtos aqui, e faça bons negócios! </p>
             </section>
            
            <section className={styles.cards}>
             <Card />
+           
             </section>
         </div>
 
