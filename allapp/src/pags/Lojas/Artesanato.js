9fa7e55d9d28551b8523b07230a9c5fd653fa1b6
Anderson Mattar <andmat7745@gmail.com>
2023-07-21 16:49:06 -0300

    inicio da pagina loja de artesanato

--- allapp/src/pags/Lojas/Artesanato.js
+++ allapp/src/pags/Lojas/Artesanato.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from './Artesanato.module.css';
+import imagem1 from './Tapetes/auto_relevo.jpeg'
 
 
 function Artesanato () {
     return(
         <section className={styles.container}>
-            <div>
-                <p>lojas</p>
-            </div>
+           <div className={styles.card}>
+            <img src={imagem1} alt="Auto Relevo" />
+            <h3>Tapete Crochê Auto Relevo</h3>
+            <span>Preço: RS 100,00</span>
+           </div>
         </section>
+        
     )
 }
 
